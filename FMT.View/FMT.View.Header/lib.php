6697487e8701dda26397d7e34041930c6e8bbd12
Abdulmumin Eneye Abdulkarim <a.e.abdulkarim@rgu.ac.uk>
2016-05-09 18:10:32 +0100

    new commit

---
+++ FMT.View/FMT.View.Header/lib.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+
+        
+        <link rel="stylesheet" href="lib/bootstrap-3.3.6-dist/bootstrap-3.3.6-dist/css/bootstrap.min.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
+        <script src="lib/bootstrap-3.3.6-dist/bootstrap-3.3.6-dist/js/bootstrap.min.js"></script>
---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Facility Management Tool</title>
+        <?php require("./FMT.View/FMT.View.Header/lib.php"); ?>
+    </head>
+    <body>
+        <?php
+            for ($i=0;$i<10;$i++){
+                //echo"<div><p>Hello World!!!</p></div>";
+            }
+        ?>
+        <div id="header">
+            <img src="FMT.Resources/FMT.Resources.Images/" style="float:left">
+            <h1 class="text-center">FACILITY MANAGEMENT</h1>
+        </div>
+    </body>
+</html>
